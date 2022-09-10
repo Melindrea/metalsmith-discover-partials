b53251975c4585a1da9c54ab8a490e0744c91b0d
Marie Hogebrandt <iam@mariehogebrandt.se>
2022-09-10 18:04:43 +0200

    Support for subdirectories and removing fs-tools

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-'use strict'
+'use strict';
 
-var defaults = require('defaults')
-var Handlebars = require('handlebars')
-var walk = require('fs-tools').walk
-var fs = require('fs')
-var path = require('path')
-
-var onFile = function (file, stats, next) {
-  fs.readFile(file, 'utf8', function (err, contents) {
-    if (err) {
-      return next(err)
-    }
-    var id = path.basename(file, path.extname(file))
-    Handlebars.registerPartial(id, contents)
-    next()
-  })
-}
+const defaults = require('defaults'),
+  Handlebars = require('handlebars'),
+  fs = require('fs'),
+  path = require('path'),
+  glob = require('glob');
 
 module.exports = function (options) {
   options = defaults(options, {
     directory: 'partials',
-    pattern: /\.hbs$/
+    pattern: '.hbs'
   })
   return function (files, metalsmith, done) {
-    walk(metalsmith.path(options.directory), options.pattern, onFile, done)
+    glob(options.directory + '/**/*' + options.pattern, function (err, files) {
+      if (err) {
+        return done(err)
+      }
+      files.forEach(file => {
+        //onFile(f, options.directory, done);
+        fs.readFile(file, 'utf8', function (err, contents) {
+          if (err) {
+            return done(err)
+          }
+          let id = file.replace(options.directory + '/', '').replace(path.extname(file), '').replace('/', '-');
+          Handlebars.registerPartial(id, contents)
+          done()
+        });
+      });
+    });
   }
-}
+};
